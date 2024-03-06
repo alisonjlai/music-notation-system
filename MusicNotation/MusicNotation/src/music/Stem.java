e987e05c0e6c34336360b4005ffa426dc54ee552
alisonjlai <alisonlaijy@gmail.com>
2024-03-05 16:34:10 -0800

    upload project

---
+++ MusicNotation/MusicNotation/src/music/Stem.java
@@ -0,0 +1,169 @@
+package music;
+
+import graphicslib.UC;
+import reaction.Gesture;
+import reaction.Reaction;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Stem extends Duration implements Comparable<Stem> {
+    public Staff staff;
+    public Head.List heads = new Head.List();
+    public boolean isUp = true;
+    public Beam beam = null;
+    public Stem(Staff staff, Head.List heads, boolean up) {
+        this.staff = staff;
+        // staff.sys.stems.addStem(this); // this is done in the Time class
+        isUp = up;
+        for(Head h: heads){h.unStem(); h.stem = this;}
+        this.heads = heads;
+        staff.sys.stems.addStem(this);
+        setWrongSides();
+
+        addReaction(new Reaction("E-E") {
+            @Override
+            public int bid(Gesture g) {
+                int y = g.vs.yM(), x1 = g.vs.xL(), x2 = g.vs.xH();
+                int xS = Stem.this.heads.get(0).time.x;
+                if (x1 > xS || x2 < xS) {return UC.noBid;}
+                int y1 = Stem.this.yLo(), y2 = Stem.this.yHi();
+                if (y < y1 || y > y2) {return UC.noBid;}
+                return Math.abs(y - (y1+y2)/2 + 100); // Allow sys.E-E to win
+            }
+
+            @Override
+            public void act(Gesture g) {Stem.this.incFlag();}
+        });
+
+        addReaction(new Reaction("W-W") {
+            @Override
+            public int bid(Gesture g) {
+                int y = g.vs.yM(), x1 = g.vs.xL(), x2 = g.vs.xH();
+                int xS = Stem.this.heads.get(0).time.x;
+                if (x1 > xS || x2 < xS) {return UC.noBid;}
+                int y1 = Stem.this.yLo(), y2 = Stem.this.yHi();
+                if (y < y1 || y > y2) {return UC.noBid;}
+                return Math.abs(y - (y1+y2)/2);
+            }
+
+            @Override
+            public void act(Gesture g) {Stem.this.decFlag();}
+        });
+    }
+    public static Stem getStem(Staff staff, Time time, int y1, int y2, boolean up){
+        Head.List heads = new Head.List();
+        for(Head h: time.heads){
+            int yH = h.y();
+            if(yH > y1 && yH < y2){heads.add(h);}
+        }
+        if(heads.size() == 0){
+            return null;
+        } // no steam created bc there's no heads
+        Beam b = internalStem(staff.sys, time.x, y1, y2);
+        Stem res = new Stem(staff, heads, up);
+        if(b != null){b.addStem(res); res.nFlag = 1;}
+        return res;
+    }
+
+    private static Beam internalStem(Sys sys, int x, int y1, int y2) { // Returns non-null if we find a beam crossed by a line
+        for(Stem s: sys.stems){
+            if(s.beam != null && s.x() < x && s.yLo() < y2 && s.yHi() > y1){
+                int bX = s.beam.first().x(), bY = s.beam.first().yBeamEnd();
+                int eX = s.beam.last().x(), eY = s.beam.last().yBeamEnd();
+                if(Beam.verticalLineCrossesSegment(x, y1, y2, bX, bY, eX, eY)){
+                    return s.beam;
+                }
+            }
+        }
+        return null;
+    }
+
+    public void show(Graphics g){
+        if (this.nFlag >= -1 && this.heads.size() > 0) {
+            int x = x(), h = staff.H(), yH = yFirstHead(), yB = yBeamEnd();    // Flag == Beam
+            g.drawLine(x, yH, x, yB);
+            if (nFlag > 0 && beam == null) {
+                if (nFlag == 1) {(isUp? Glyph.FLAG1D:Glyph.FLAG1U).showAt(g, h, x, yB);}
+                if (nFlag == 2) {(isUp? Glyph.FLAG2D:Glyph.FLAG2U).showAt(g, h, x, yB);}
+                if (nFlag == 3) {(isUp? Glyph.FLAG3D:Glyph.FLAG3U).showAt(g, h, x, yB);}
+                if (nFlag == 4) {(isUp? Glyph.FLAG4D:Glyph.FLAG4U).showAt(g, h, x, yB);}
+            }
+        }
+    }
+    public Head firstHead(){return heads.get(isUp? heads.size()-1 : 0);}    // Either the first or the last
+    public Head lastHead(){return heads.get(isUp? 0 : heads.size()-1);}
+    public int yFirstHead(){Head h = firstHead(); return h.staff.yLine(h.line);}
+    public boolean isInternalStem(){
+        return beam != null && beam.stems != null && this != beam.first() && this != beam.last();
+    }
+    public int yBeamEnd(){
+        if(isInternalStem()){beam.setMasterBeam(); return Beam.yOfX(x());}
+        Head h = lastHead();
+        int line = h.line;
+        line += isUp? - 7 : 7;    // Up stem or down stem
+        //increase more if more flags has been added
+        //if flag is more than 2, give room for the extra flag(s)
+        int flagInc = nFlag > 2? 2*(nFlag-2) : 0;
+        line += isUp? - flagInc : flagInc;
+        //Head towards center
+        if ((isUp && line > 4) || (!isUp && line < 4)) {line = 4;}
+        return h.staff.yLine(line);
+    }
+    public int x(){Head h = firstHead(); return h.time.x + (isUp? h.W() : 0);}
+
+    public int yLo(){return isUp? yBeamEnd(): yFirstHead();}
+    public int yHi(){return isUp? yFirstHead(): yBeamEnd();}
+
+    public void deleteStem() {
+        staff.sys.stems.remove(this);
+        deleteMass();
+    }
+
+    public void setWrongSides() {    // Stub - fixed
+        Collections.sort(heads);
+        int i, last, next;
+        if (isUp){
+            i = heads.size() - 1; last = 0; next = -1;
+        }else{
+            i = 0;
+            last = heads.size() - 1;
+            next = 1;
+        }
+        Head ph = heads.get(i);     // previous head
+        ph.wrongSide = false;
+        while (i != last){
+            i += next;
+            Head nh = heads.get(i);   // next head
+            nh.wrongSide = ph.staff == nh.staff && (Math.abs(nh.line - ph.line) <= 1 && ! ph.wrongSide);
+            ph = nh;
+        }
+    }
+
+    @Override
+    public int compareTo(Stem s) {return x() - s.x();}
+
+    //----------------------------------List---------------------------------------
+
+    public static class List extends ArrayList<Stem>{
+        public int yMin = 1_000_000, yMax = -1_000_000;
+        public void addStem(Stem s) {
+            add(s);
+            if (s.yLo() < yMin) {yMin = s.yLo();}
+            if (s.yHi() > yMax) {yMax = s.yHi();}
+        }
+
+        public boolean fastReject(int y1, int y2){return false;} //y2 < yMin || y1 > yMax;}
+
+        public void sort(){Collections.sort(this);}
+        public Stem.List allIntersecters(int x1, int y1, int x2, int y2){
+            Stem.List res = new Stem.List();
+            for(Stem s: this){
+                int x = s.x(), y = Beam.yOfX(x, x1, y1, x2, y2);
+                if(x > x1 && x < x2 && y > s.yLo() && y < s.yHi()){res.add(s);}
+            }
+            return res;
+        }
+    }
+}
---
+++ MusicNotation/MusicNotation/src/music/Sys.java
@@ -0,0 +1,100 @@
+package music;
+
+import graphicslib.UC;
+import reaction.Gesture;
+import reaction.Ink;
+import reaction.Mass;
+import reaction.Reaction;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+import static music.AppMusicEd.PAGE;
+
+public class Sys extends Mass {
+    public Staff.List staffs = new Staff.List();
+    public Page page = PAGE;
+    public int iSys;
+    public Sys.Fmt fmt;
+    public Time.List times;
+    public Stem.List stems = new Stem.List();
+
+
+    public Sys(int iSys, Sys.Fmt sysFmt) {
+        super("BACK");
+        this.iSys = iSys;
+        this.fmt = sysFmt;
+        this.times = new Time.List(this);
+        for (int i = 0; i < sysFmt.size(); i++){
+            addStaff(new Staff(i, sysFmt.get(i)));
+        }
+
+        addReaction(new Reaction("E-E") { // Beam and stems
+            @Override
+            public int bid(Gesture g) {
+                int x1 = g.vs.xL(), y1 = g.vs.yL(), x2 = g.vs.xH(), y2 = g.vs.yH();
+                System.out.println("Before fastReject");
+                if(stems.fastReject(y1, y2)){return UC.noBid;}
+                Stem.List temp = stems.allIntersecters(x1, y1, x2, y2);
+                System.out.println("Intersected" + temp.size());
+                if(temp.size() < 2){return UC.noBid;}
+                Beam beam = temp.get(0).beam; // Check all crossed stems owned by same beam
+                for (Stem s: temp) {
+                    if(s.beam != beam) {return UC.noBid;}
+                }
+                if(beam == null && temp.size() != 2){return UC.noBid;}
+                if(beam == null && (temp.get(0).nFlag != 0 || temp.get(1).nFlag != 0)){return UC.noBid;}
+                return 50;
+            }
+
+            @Override
+            public void act(Gesture g) {
+                int x1 = g.vs.xL(), y1 = g.vs.yL(), x2 = g.vs.xH(), y2 = g.vs.yH();
+                Stem.List temp = stems.allIntersecters(x1, y1, x2, y2);
+                Beam beam = temp.get(0).beam;
+                if(beam == null){new Beam(temp.get(0), temp.get(1));}else{
+                    for(Stem s: temp){s.incFlag();}
+                }
+
+            }
+        });
+    }
+    public Time getTime(int x) {return times.getTime(x);}
+    public int yTop(){return page.sysTop(iSys);}
+    public int yBot(){return staffs.get(staffs.size() - 1).yBot();}
+    public void addStaff(Staff s){
+        staffs.add(s);
+        s.sys = this;
+    }
+
+    @Override
+    public void show(Graphics g) {
+        int y = yTop(), x = PAGE.margins.left;
+        g.drawLine(x, y, x, y + fmt.height());
+    }
+
+    //--------------------------------Fmt--------------------------------------------
+    public static class Fmt extends ArrayList<Staff.Fmt> {
+        public int maxH = 0;
+        public ArrayList<Integer> staffOffset = new ArrayList<>();
+
+        public int height() {int last = size() - 1; return staffOffset.get(last) + get(last).height();}
+        public void showAt(Graphics g, int y){
+            for (int i = 0; i < size(); i++){
+                get(i).showAt(g, y + staffOffset.get(i));
+            }
+        }
+
+        public void addStaffFmt(Staff.Fmt sf, int yOff) {
+            add(sf);
+            staffOffset.add(yOff);
+            if(maxH < sf.H){
+                maxH = sf.H;
+            }
+        }
+    }
+    //--------------------------------List--------------------------------------------
+    public static class List extends ArrayList<Sys>{
+
+    }
+}
